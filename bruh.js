27080c18e50693be03b1c383d8ef4ab07b692a1a
Yogi <78745590+KislayMishra69@users.noreply.github.com>
2022-05-04 15:18:00 +0530

    Add files via upload

---
+++ bruh.js
@@ -0,0 +1,9 @@
+console.log('Bsdk madarchod bkl');
+console.log('This Website belongs to Kislay Mishra.')
+
+var typed = new Typed(".typing", {
+    strings: ['Student.','Learner.', 'Programmer.', 'Discord Bot Developer.', 'Video Editor.', 'Web Developer.', 'Game Developer.'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+})
